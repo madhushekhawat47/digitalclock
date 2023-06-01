92c4b84d7c9081a2412a12efa22014b779ff8d7e
madhushekhawat47 <94750729+madhushekhawat47@users.noreply.github.com>
2023-06-01 23:13:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, {  useState } from 'react';
+
+const App=()=>
+{
+    let Time=new Date().toLocaleTimeString();
+    
+    const[ctime,setCtime]=useState(Time);
+    const UpdateTime=()=>
+    {
+        let Time=new Date().toLocaleTimeString();
+        setCtime(Time);
+    };
+    setInterval(UpdateTime,1000)
+    return(
+        <>
+            <h1>{ctime}</h1>
+        </>
+    );
+};
+export default App;
